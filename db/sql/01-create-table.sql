18289eeb695adb0699c3fc3534bd484993cb7844
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2021-03-21 21:39:33 -0400

    Add files via upload

---
+++ db/sql/01-create-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `animal`.bird (
+  id int PRIMARY KEY AUTO_INCREMENT,
+  name varchar(20) NOT NULL,
+  hasFeathers int NOT NULL,
+  isFlyable int NOT NULL
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_general_ci;
---
+++ db/sql/02-insert-table.sql
@@ -0,0 +1,7 @@
+INSERT INTO `animal`.bird (id, name, hasFeathers, isFlyable)
+VALUES
+( default, 'ag', 0, 0),
+( default, 'au', 0, 0),
+( default, 'be', 0, 1),
+( default, 'br', 1, 0),
+( default, 'ca', 1, 1);
